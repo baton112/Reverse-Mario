fd88d901fee7a10092ec53386c2d2025a04fe28a
baton112 <baton112@op.pl>
2014-07-15 17:00:21 +0200

    Creation v2

---
+++ android/src/com/mygdx/game/mario/revrse/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.game.mario.revrse.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.game.mario.revrse.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
